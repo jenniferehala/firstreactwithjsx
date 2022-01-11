65e656335a473615b5ad3e45e54f87cdbebf6721
jehala <jenniferehala@github.com>
2022-01-11 13:05:30 -0500

    commit

---
+++ src/components/page.js
@@ -0,0 +1,22 @@
+import React, {Component} from "react"
+
+class Page extends Component {
+    render() {
+        return (
+            <div>            
+                <h1>Hello Dojo!</h1>
+                <p> Things i need to do:
+                    <ul>
+                        <li>Learn React</li>
+                        <li>Climb Mt Everest</li>
+                        <li>Run a marathon</li>
+                        <li>Feed the dogs</li>
+                    </ul>
+                </p>
+            </div>
+        );
+    }
+}
+
+
+export default Page 
